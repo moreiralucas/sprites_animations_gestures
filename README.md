fb5023b889b43dfc0d9011fb8a73fac767572b85
Lucas Moreira <lucas-mopi@hotmail.com>
2021-04-24 17:46:30 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sprites Animations Gestures
+
+A simple project flutter build with [Flame](https://github.com/flame-engine/flame).
+
---
+++ android/app/src/main/kotlin/com/example/sprites_animations_gestures/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sprites_animations_gestures
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
